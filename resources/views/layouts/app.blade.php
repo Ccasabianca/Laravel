b17d72a604bb175d2531a822c8fe56558ed19963
Ccasabianca <107222996+Ccasabianca@users.noreply.github.com>
2025-12-16 14:49:41 +0100

    ACTIVITE 15 : Ajouter la favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!-- resources/views/layouts/app.blade.php -->
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<link rel="icon" href="{{ asset('favicon.png') }}">
 
 <head>
     <meta charset="UTF-8">
